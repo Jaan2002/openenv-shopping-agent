ea1e1ffd5699d9177417a3d75ef279dd94049f5a
Jaanvi <84656942+Jaan2002@users.noreply.github.com>
2026-04-08 09:03:44 +0530

    Add files via upload

---
+++ server/app.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from env import ShoppingEnv
+from models import Action
+from inference import run
+
+app = FastAPI()
+env = ShoppingEnv()
+
+
+@app.get("/")
+def home():
+    return {"message": "Shopping AI Env Running"}
+
+
+@app.post("/reset")
+def reset():
+    obs = env.reset()
+    return obs.dict()
+
+@app.post("/step")
+def step(action: dict):
+    act = Action(**action)
+    obs, reward, done, info = env.step(act)
+    return {
+        "observation": obs.dict(),
+        "reward": reward.score,
+        "done": done,
+        "info": info
+    }
+
+@app.get("/state")
+def state():
+    return {"status": "running"}
+
+@app.on_event("startup")
+def run_inference_on_start():
+    run()
