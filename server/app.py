d38d4150c47ebedda5253af0afdab6872dd0f4d9
Jaanvi <84656942+Jaan2002@users.noreply.github.com>
2026-04-12 23:36:43 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,17 +1,15 @@
 from openenv.core.env_server import create_fastapi_app
-from server.environment import ShoppingEnv, TaskAction, TaskObservation
+from server.env import ShoppingEnv, Action, Observation
 
 app = create_fastapi_app(
     ShoppingEnv,
-    TaskAction,
-    TaskObservation
+    Action,
+    Observation
 )
-
 
 def main():
     import uvicorn
     uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
-
 
 if __name__ == "__main__":
     main()
